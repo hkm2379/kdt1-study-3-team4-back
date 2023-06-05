38235618e8d762fa7037330ffadec0d06b63b7fa
hkm2379 <129468128+hkm2379@users.noreply.github.com>
2023-06-05 17:16:41 +0900

    Update CorsConfig.java

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:8080") //vue는 허용하겠다는 소리
                 .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://15.164.206.68")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
